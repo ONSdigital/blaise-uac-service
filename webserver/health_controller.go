b5b491f91b0929cd7100a82722e7fd86204f0476
Sam Bryant <sam.bryant@armakuni.com>
2021-07-15 09:17:14 +0100

    Add bus/version/health endpoint

--- webserver/health_controller.go
+++ webserver/health_controller.go
@@ -8,13 +8,18 @@ import (
 
 type Health struct {
 	Healthy bool `json:"healthy"`
+	Version string `json:"version,omitempty"`
 }
 
 type HealthController struct {
 }
 
 func (healthController *HealthController) AddRoutes(httpRouter *gin.Engine) {
-	httpRouter.GET("/health", func(context *gin.Context) {
-		context.JSON(http.StatusOK, Health{Healthy: true})
-	})
+	httpRouter.GET("/health", healthController.HealthEndpoint)
+	httpRouter.GET("/bus/:version/health", healthController.HealthEndpoint)
+}
+
+func (healthController *HealthController) HealthEndpoint(context *gin.Context) {
+	version := context.Param("version")
+	context.JSON(http.StatusOK, Health{Healthy: true, Version: version})
 }
