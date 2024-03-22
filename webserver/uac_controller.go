38c9c0d4611c378900a1402203443733c3bf4dbe
elthorne <46898565+elthorne@users.noreply.github.com>
2024-03-22 09:11:27 +0000

    debug: DO NOT MERGE TO MAIN
    various debug statements added
    
    refs: BLAIS5-4130

--- webserver/uac_controller.go
+++ webserver/uac_controller.go
@@ -48,8 +48,11 @@ func (uacController *UacController) AddRoutes(httpRouter *gin.Engine) {
 
 func (uacController *UacController) UACInstrumentGenerateEndpoint(context *gin.Context) {
 	instrumentName := context.Param("instrumentName")
+    log.Println("EL'S DEBUG: Got instrumentName -> ", instrumentName)
 	instrumentModes, err := uacController.BlaiseRestApi.GetInstrumentModes(instrumentName)
 	if err != nil {
+	    log.Println("EL'S DEBUG: failed to get instrument modes -> ", err)
+	    log.Println("EL'S DEBUG: Got instrumentModes -> ", instrumentModes)
 		uacController.blaiseRestApiError(context, err)
 		return
 	}
@@ -59,16 +62,21 @@ func (uacController *UacController) UACInstrumentGenerateEndpoint(context *gin.C
 	}
 	caseIDs, err := uacController.BlaiseRestApi.GetCaseIds(instrumentName)
 	if err != nil {
+        log.Println("EL'S DEBUG: failed to get caseIds -> ", err)
+        log.Println("EL'S DEBUG: Got caseIds -> ", caseIDs)
 		uacController.blaiseRestApiError(context, err)
 		return
 	}
 	err = uacController.UacGenerator.Generate(instrumentName, caseIDs)
 	if err != nil {
+        log.Println("EL'S DEBUG: failed to get generate UACs -> ", err)
 		_ = context.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	uacs, err := uacController.UacGenerator.GetAllUacs(instrumentName)
 	if err != nil {
+        log.Println("EL'S DEBUG: failed to get all UACs -> ", err)
+        log.Println("EL'S DEBUG: Got UACs -> ", UACs)
 		_ = context.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
