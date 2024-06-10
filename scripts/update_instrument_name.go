e14e64c7bd0704234197c979cd1fa4c3f6cfd2c1
lambeb <benjamin.lambe@ons.gov.uk>
2024-06-10 16:58:15 +0100

    Added a script to enable or disable UAC's. Not tested yet

--- scripts/update_instrument_name.go
+++ scripts/update_instrument_name.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"cloud.google.com/go/datastore"
 	"context"
 	"fmt"
+	"google.golang.org/api/iterator"
 	"log"
 	"os"
-	"cloud.google.com/go/datastore"
-	"google.golang.org/api/iterator"
 )
 
 type dsEntityStruct struct {
 	InstrumentName string `json:"instrument_name" datastore:"instrument_name"`
-	CaseID string `json:"case_id" datastore:"case_id"`
-	Disabled *bool `json:"disabled" datastore:"disabled"`
+	CaseID         string `json:"case_id" datastore:"case_id"`
+	Disabled       bool   `json:"disabled" datastore:"disabled"`
 }
 
 func main() {
