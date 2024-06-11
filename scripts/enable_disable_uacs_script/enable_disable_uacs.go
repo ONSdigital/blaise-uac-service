971f3c006598060b5c920ee0cfd7280ad4eb2020
lambeb <benjamin.lambe@ons.gov.uk>
2024-06-11 11:26:15 +0100

    This is working, but not very good code. GCP DataStore doesn't support the in operator, so would have to do each query separately. This will work in the mean time, but is not very efficient.

--- scripts/enable_disable_uacs_script/enable_disable_uacs.go
+++ scripts/enable_disable_uacs_script/enable_disable_uacs.go
@@ -20,68 +20,32 @@ type dsEntityStruct struct {
 func main() {
 
 	projectID := os.Getenv("PROJECT_ID")
-	uacsToEnable := os.Getenv("UACS_TO_ENABLE")
-	uacsToDisable := os.Getenv("UACS_TO_DISABLE")
+	uacsToEnableItems := strings.Split(os.Getenv("UACS_TO_ENABLE"), ",")
+	uacsToDisableItems := strings.Split(os.Getenv("UACS_TO_DISABLE"), ",")
 
-	uacEnableCount := 0
 	uacEnableUpdatedCount := 0
-
-	uacDisableCount := 0
 	uacDisableUpdatedCount := 0
 
-	enabled_uac_list := strings.Split(uacsToEnable, ",")
-	disabled_uac_list := strings.Split(uacsToDisable, ",")
-
 	ctx := context.Background()
-
 	dsClient, err := datastore.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer dsClient.Close()
-
-	dsEnableQuery := datastore.NewQuery("uac").FilterField("Name/ID", "in", enabled_uac_list)
-	dsDisableQuery := datastore.NewQuery("uac").FilterField("Name/ID", "in", disabled_uac_list)
-
-	dsEnableIt := dsClient.Run(ctx, dsEnableQuery)
-	dsEnableItCount := *dsEnableIt
-
-	dsDisableIt := dsClient.Run(ctx, dsDisableQuery)
-	dsDisableItCount := *dsDisableIt
-
-	for {
-		var entity dsEntityStruct
-		_, err := dsEnableItCount.Next(&entity)
-		if err == iterator.Done {
-			break
-		}
+	defer func(dsClient *datastore.Client) {
+		err := dsClient.Close()
 		if err != nil {
-			log.Fatal(err)
-		}
-		uacEnableCount++
-	}
 
-	fmt.Println("Found", uacEnableCount, "UACs to enable")
+		}
+	}(dsClient)
+
+	dsQuery := datastore.NewQuery("uac")
+
+	dsIterator := dsClient.Run(ctx, dsQuery)
 
 	for {
 		var entity dsEntityStruct
-		_, err := dsDisableItCount.Next(&entity)
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		uacDisableCount++
-	}
 
-	fmt.Println("Found", uacDisableCount, "UACs to disable")
-
-	for {
-
-		var entity dsEntityStruct
-
-		key, err := dsEnableIt.Next(&entity)
+		key, err := dsIterator.Next(&entity)
 		if err == iterator.Done {
 			break
 		}
@@ -89,51 +53,40 @@ func main() {
 			log.Fatal(err)
 		}
 
-		dsEntityKey := datastore.NameKey("uac", key.Name, nil)
+		for _, uac := range uacsToEnableItems {
+			if key.Name == uac {
 
-		fmt.Println("Enabling UAC", key.Name)
+				dsEntityKey := datastore.NameKey("uac", key.Name, nil)
+				entity.Disabled = false
 
-		entity.Disabled = false
+				if _, err := dsClient.Put(ctx, dsEntityKey, &entity); err != nil {
+					log.Fatal(err)
+				}
 
-		if _, err := dsClient.Put(ctx, dsEntityKey, &entity); err != nil {
-			log.Fatal(err)
+				fmt.Println("Enabled UAC", key.Name)
+				uacEnableUpdatedCount++
+				break
+			}
 		}
 
-		fmt.Println("Enabled UAC", key.Name)
+		for _, uac := range uacsToDisableItems {
+			if key.Name == uac {
+				dsEntityKey := datastore.NameKey("uac", key.Name, nil)
 
-		uacEnableUpdatedCount++
+				entity.Disabled = true
+
+				if _, err := dsClient.Put(ctx, dsEntityKey, &entity); err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("Disabled UAC", key.Name)
+				uacDisableUpdatedCount++
+				break
+			}
+		}
 
 	}
 
 	fmt.Println("Enabled", uacEnableUpdatedCount, "UACs")
-
-	for {
-
-		var entity dsEntityStruct
-
-		key, err := dsDisableIt.Next(&entity)
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		dsEntityKey := datastore.NameKey("uac", key.Name, nil)
-
-		fmt.Println("Disabling UAC", key.Name)
-
-		entity.Disabled = true
-
-		if _, err := dsClient.Put(ctx, dsEntityKey, &entity); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Disabled UAC", key.Name)
-
-		uacDisableUpdatedCount++
-
-	}
-
 	fmt.Println("Disabled", uacDisableUpdatedCount, "UACs")
 }
